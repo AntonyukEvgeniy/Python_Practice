a45bda87c2450d3c20060d0d9dd79c3c4c0cbcab
Антонюк Евгений <38914583+AntonyukEvgeniy@users.noreply.github.com>
2024-09-09 12:58:06 +0300

    Домашняя работа 9.1 Добавлены файлы проекта. Добавлен модуль masks

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+print(get_mask_card_number(2202208131326309))
+print(get_mask_account(2202208131326309))
---
+++ src/masks.py
@@ -0,0 +1,26 @@
+def get_mask_card_number(card_number: int|str) -> str:
+    """
+    Принимает на вход номер карты и возвращает ее маску. Номер карты замаскирован и отображается в формате:
+    XXXX XX** **** XXXX
+    """
+    card_text_view = str(card_number)
+    if not card_text_view.isdigit():
+        raise ValueError("Введен неверный номер карты")
+    if len(card_text_view) != 16:
+        raise ValueError("Неверный формат номера карты")
+    card_formated_view = (
+        f"{card_text_view[0:4]} {card_text_view[4:6]}** **** {card_text_view[-4:]}"
+    )
+    return card_formated_view
+
+
+def get_mask_account(bank_account_number: int|str) -> str:
+    """
+    Принимает на вход номер счета и возвращает его маску. Номер счета замаскирован и отображается в формате
+    **XXXX
+    """
+    bank_account_text_view = str(bank_account_number)
+    if not bank_account_text_view.isdigit():
+        raise ValueError("Введен неверный номер счета")
+    bank_account_formated = f"**{bank_account_text_view[-4:]}"
+    return bank_account_formated
