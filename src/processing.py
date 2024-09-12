a82e2f60f35655f022f7fab251970fb60c870624
Антонюк Евгений <38914583+AntonyukEvgeniy@users.noreply.github.com>
2024-09-12 15:46:45 +0300

    Добавлен модуль "processing", в котором реализованы 2 функции filter_by_state и sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,15 @@
+def filter_by_state(*,transactions: list[dict], state="EXECUTED") -> list[dict]:
+    """
+    Принимает список словарей и опционально значение для ключа state.
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ равен state.
+    """
+    filtred_transactions = [transaction for transaction in transactions if transaction["state"] == state]
+    return filtred_transactions
+
+
+def sort_by_date(*,transactions: list[dict], sort_order=True) -> list[dict]:
+    """
+    Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате.
+    """
+    return sorted(transactions, key=lambda transaction: transaction["date"], reverse=sort_order)
