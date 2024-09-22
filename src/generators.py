cba46055b509c895c02f25de7856d018ea94bfdc
Антонюк Евгений <38914583+AntonyukEvgeniy@users.noreply.github.com>
2024-09-23 01:43:00 +0300

    Покрытие тестами модуля generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,50 +1,45 @@
-from collections.abc import Callable
-from email.generator import Generator
-from operator import index
 from typing import Iterator
-
-from tests.conftest import transactions
 
 
 def filter_by_currency(transactions: list[dict], currency: str) -> Iterator[dict]:
     """
-    Создайте функцию
-    filter_by_currency
-    ,которая принимает на вход список словарей, представляющих транзакции.
-    Функция должна возвращать итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной (например, USD).
+    Принимает на вход список словарей, представляющих транзакции.
+    Функция возвращает итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной (например, USD).
     """
     transactions_by_currency = list(
         filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions)
     )
+    if len(transactions_by_currency) == 0:
+        yield {}
     for tran in transactions_by_currency:
         yield tran
 
 
 def transaction_descriptions(transactions: list[dict]) -> Iterator[str]:
     """
-    Генератор transaction_descriptions, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
+    Генератор transaction_descriptions, который принимает список словарей с транзакциями
+    и возвращает описание каждой операции по очереди.
     """
+    if len(transactions) == 0:
+        yield "Нет транзакций"
     for tran in transactions:
         yield tran["description"]
 
 
-def card_number_generator(start:int, stop:int)-> Iterator[str]:
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
     """
-        Создайте генератор
-    card_number_generator
-    , который выдает номера банковских карт в формате
-    XXXX XXXX XXXX XXXX
-    , где
-    X
-     — цифра номера карты. Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX, где X
+     — цифра номера карты.
+    Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
     Генератор должен принимать начальное и конечное значения для генерации диапазона номеров.
     """
+    if start < 1 or start > 9999999999999999 or stop < 1 or stop > 9999999999999999 or stop < start:
+        raise ValueError("Невозможный диапазон номера карты")
     credit_card_number = "0000000000000000"
-    for n in range(start, stop+1, 1):
+    for n in range(start, stop + 1, 1):
         change_len = 16 - len(str(n))
         credit_card_number_new = f"{credit_card_number[0:change_len]}{n}"
-        credit_card_number_new_formatted = f"{credit_card_number_new[0:4]} {credit_card_number_new[4:8]} {credit_card_number_new[8:12]} {credit_card_number_new[12:]}"
+        credit_card_number_new_formatted = (f"{credit_card_number_new[0:4]} {credit_card_number_new[4:8]}"
+                                            f" {credit_card_number_new[8:12]} {credit_card_number_new[12:]}")
         yield credit_card_number_new_formatted
-
-for card_number in card_number_generator(1, 5):
-    print(card_number)
